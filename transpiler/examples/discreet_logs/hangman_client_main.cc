559cb5558b6ccaf838124be45a8067145c5eee7c
Samuel Ballan <sgb4622@gmail.com>
2022-04-27 20:53:52 +0000

    examples and tests

--- transpiler/examples/discreet_logs/hangman_client_main.cc
+++ transpiler/examples/discreet_logs/hangman_client_main.cc
@@ -48,11 +48,55 @@ int main() {
   std::string input;
   getline(std::cin, input);
 
-  auto ciphertext = TfheString::Encrypt(input, key);
-  std::cout << "Encryption done" << std::endl;
+  database_titles = ""
+  index_titles = {}
+  database_contents = ""
+  index_contents = {}
 
-  std::cout << "Initial state check by decryption: " << std::endl;
-  std::cout << ciphertext.Decrypt(key) << "\n";
+  indexes = {0, 21, 31, 45}
+
+  query = ["My new note", "This is my amazing note"]
+
+  database_titles += query[0]
+  index_title.insert(database_titles.length())
+
+  // after
+  database_titles = "My new note"
+  index_titles = {11}
+
+
+
+
+  // std::array<std::string, 2> input_array = {input, input};
+
+  // std::vector<int32_t> int_vector = {1, 2, 3};
+  // std::vector<std::vector<char>> string_vector = {"hello", "world"};
+
+  // auto int_array0 = TfheArray<std::vector<char>>::Encrypt(string_vector, key);
+
+  // auto int_array1 = TfheArray<int32_t>::Encrypt(int_vector, key);
+
+  // auto int_array = TfheArray<int32_t>::Encrypt({1, 2, 3}, key);
+
+  // auto ciphertext = TfheArray<int>::Encrypt(int_vector, key)
+
+
+  // auto ciphertext = TfheStringArray::Encrypt(input_array, key);
+  
+
+  auto ciphertext = TfheString::Encrypt(input, key);
+  // std::cout << "Encryption done" << std::endl;
+
+  // std::cout << "Initial state check by decryption: " << std::endl;
+  // std::cout << ciphertext.Decrypt(key) << "\n";
+
+  // int32_t output[3] = {};
+  auto int_array3 = TfheArray<int32_t>::Encrypt({1, 2, 3, 4}, key);
+  auto decoded = int_array3.Decrypt(key);
+  for (int i = 0; i < 4; i++) {
+    std::cout << decoded[i] << "\n";
+  }
+  // std::cout << int_array1.Decrypt(key) << "\n";
   std::cout << "\n";
 
   // Make a move.
