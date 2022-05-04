e1824f289a9be20092e0384c229e3b8eebe7084d
Samuel Ballan <sgb4622@gmail.com>
2022-05-04 18:48:14 +0000

    testing a larger implementation

--- transpiler/examples/discreet_logs/hangman_api.cc
+++ transpiler/examples/discreet_logs/hangman_api.cc
@@ -1,25 +1,45 @@
-// Copyright 2021 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 #include "hangman_api.h"
 
+// Rules:
+// database contains title and contents in a continuous sequence of pairs. 
+// idxs contain indexes of where the titles and note contents start and stop.
+// The first integer in idxs indicates where the first title stops.  The second
+// integer indicates where the first content stops.
+//
+// all arrays must have a fixed size known at compile time.  All loops must have
+// a known number of iterations at compile time.
+
 #pragma hls_top
-void hangmanMakeMove(char database[4], char query[1], char result[1]) {
-#pragma hls_unroll yes
-  for (int i = 0; i < 4; i+=2) {
-    if(query[0] == database[i]) {
-      result[0] = database[i+1];
+void hangmanMakeMove(char database[8], int idxs[8], char query[8], char result[8]) {
+  #pragma hls_unroll yes
+  for (int i = 0; i < 8; i+=2) {
+    // We use -1 to signify the end of this Array
+    if(idxs[i] == -1) break;
+
+    bool found = false;
+    int current_idx = idxs[i];
+    int end_idx = idxs[i+1];
+
+    #pragma hls_unroll yes
+    // for (int j = idxs[i]; j < idxs[i+1]; j+=1) {
+    for (int j = 0; j < 8; j+=1) {
+      if(current_idx >= end_idx) break;
+
+      if (query[current_idx] == database[current_idx]) {
+        if(current_idx==end_idx) {
+          found = true;
+          break;
+        }
+        else {
+          current_idx+=1;
+          continue;
+        }
+      } else break;    
     }
+
+    if(found) {
+
+    }
+
   }
 }
