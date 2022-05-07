fe5c64a082be59211b0f39917cdf732d63b29dc7
Samuel Ballan <sgb4622@gmail.com>
2022-05-07 19:00:53 +0000

    something works

--- transpiler/examples/discreet_logs/discreet_logs_select_index.cc
+++ transpiler/examples/discreet_logs/discreet_logs_select_index.cc
@@ -2,45 +2,20 @@
 
 #pragma hls_top
 void selectIndex(
-  char db[MAX_ARRAY_SIZE], 
-  short db_idx[MAX_ARRAY_SIZE], 
-  char query[MAX_ARRAY_SIZE], 
-  short query_params[2], 
-  short result[1]
+  char record[MAX_SIZE], 
+  char query[MAX_SIZE], 
+  int result[1]
 ) {
-  // Which db index are we on
-  short current_db_index=0;
-  short cursor = 0;
-  short key_end = db_idx[current_db_index];
+  if(result[0] == 1){
+    return;
+  } 
 
-  short found_index = -1;
-
-  // Iterate over the db_idx, to look at keys
 #pragma hls_unroll yes
-  for(short i=0; i<MAX_ARRAY_SIZE; i++) {
-    // We use -1 in the db_idx to indicate null values
-    // if(db_idx[current_db_index] == -1) break;
-
-    // Break if we know we'll read empty data (and seg fault)
-    // if (cursor >= query_params[1]) break;
-
-    // result[0] += 1;
-
-    if(query[i] == 'a'){
-      // In this condition, a character didn't match the query.
-      // Discard this key comparison, and go to the next key
-      // current_db_index += 2;
-      // if(db_idx[current_db_index] == -1) break;
-
-      // cursor = db_idx[current_db_index];
-      // key_end = db_idx[current_db_index+1];
-      result[0] = i;
-      cursor++;
-    } else if (query[i] == 'b'){
-      break;
+  for(int i=0; i<MAX_SIZE/2; i++) {
+    if(query[i] != record[i]) {
+      result[0] = 0;
+      return;
     }
-    
   }
-
-  // result[0] = cursor;
+  result[0] == 1;
 }
