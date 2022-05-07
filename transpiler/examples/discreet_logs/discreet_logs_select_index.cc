26f0a07dcfe823299391591caa6f45d8256700eb
Samuel Ballan <sgb4622@gmail.com>
2022-05-07 20:40:00 +0000

    match is working!

--- transpiler/examples/discreet_logs/discreet_logs_select_index.cc
+++ transpiler/examples/discreet_logs/discreet_logs_select_index.cc
@@ -12,7 +12,7 @@ void selectIndex(
   } 
 
 #pragma hls_unroll yes
-  for(int i=0; i<3; i++) {
+  for(int i=0; i<MAX_SIZE/2; i++) {
     if(query[i] != record[i]) {
       result[0] = 0;
       return;
