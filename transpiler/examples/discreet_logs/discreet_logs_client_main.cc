acdb61a85a47d68af8ca2caeb529a351e7d074f3
Samuel Ballan <sgb4622@gmail.com>
2022-05-09 01:57:42 +0000

    new client server messages

--- transpiler/examples/discreet_logs/discreet_logs_client_main.cc
+++ transpiler/examples/discreet_logs/discreet_logs_client_main.cc
@@ -61,13 +61,17 @@ int main() {
   // The order of these can be randomized so we don't know all blank ciphers are at the end.
   TfheString demo_record = TfheString::Encrypt(raw_demo_record, key);
   TfheString blank_record1 = TfheString::Encrypt(raw_blank_record, key);
+
+  std::cout << "Initial database records have been created and encrypted.  Sending records and cloud key to the server..." << std::endl;
   
   std::vector<TfheString*> db;
   db.push_back(&demo_record);
   db.push_back(&blank_record1);
 
-  // records are seralized, sent to carol, and persisted.  all future accesses of records happen by carol
+  std::cout << "Server has received and persisted the encrypted records and the cloud key." << std::endl;
 
+
+  // records are seralized, sent to carol, and persisted.  all future accesses of records happen by carol
   std::cout << "Database handshake complete\n\n" << std::endl;
 
   std::cout << "Welcome to Discreet Logs!" << std::endl;
@@ -95,13 +99,23 @@ int main() {
       auto counter = TfheArray<int>::Encrypt(raw_counter, key);
       auto result = TfheString::Encrypt(raw_result, key);
 
+      std::cout << "\n-- READ_ALL operation, counter ciphertext, and result ciphertext, all sent to server" << std::endl;
+
       for(int i=0; i<db.size();i++) {
         XLS_CHECK_OK(readAll(*db[i], counter, result, key.cloud()));
       }
 
+      std::cout << "-- Server returns altered result ciphertext" << std::endl;
+
       auto output = result.Decrypt(key);
 
-      std::cout << output;
+      std::cout << "Response: \"";
+
+      for(int i=0; i< READ_ALL_MAX*READ_ALL_COUNT;i++) {
+        std::cout << output[i];
+      }
+
+      std::cout << "\"";
       std::cout << "\n" << std::endl;      
     } else if(op_type == 2) {
       char raw_result[SELECT_INDEX_MAX/2+1];
@@ -117,7 +131,7 @@ int main() {
       std::cin >> input;
       std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-      std::cout << "*" << input << "*" << std::endl;
+      std::cout << "Entered \"" << input << "\"" << std::endl;
 
       for(int i=0; i<SELECT_INDEX_MAX/2; i++) {
         if(i < input.size() && input[i] != '\n') {
@@ -130,13 +144,21 @@ int main() {
       auto query = TfheString::Encrypt(input_chars, key);
       auto result = TfheString::Encrypt(raw_result, key);
 
+      std::cout << "\n-- SELECT operation, query ciphertext, and result ciphertext, all sent to server" << std::endl;
+
       for(int i=0; i<db.size();i++) {
         XLS_CHECK_OK(selectIndex(*db[i], query, result, key.cloud()));
       }
 
+      std::cout << "-- Server returned altered result ciphertext\n" << std::endl;
+
       auto output = result.Decrypt(key);
 
-      std::cout << output;
+      std::cout << "Response: \"";
+      for(int i=0; i< SELECT_INDEX_MAX;i++) {
+        std::cout << output[i];
+      }
+      std::cout << "\"";
       std::cout << "\n" << std::endl;
     } else if(op_type == 3) {
       int raw_result[1+1] = {0,0};
@@ -161,15 +183,14 @@ int main() {
           input_chars[i] = 0; 
         }      
       }
-      std::cout << "*" << input_chars << "*" << std::endl;
+      std::cout << "Entered \"" << input_chars << "*" << std::endl;
 
-      std::cout << "Please enter the content (max size "<< INSERT_MAX/2 <<" chars): \n"  << std::endl;
+      std::cout << "\nPlease enter the content (max size "<< INSERT_MAX/2 <<" chars): \n"  << std::endl;
       
       std::string input2;
       // std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
       std::cin >> input2;
       // std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-      // std::cout << "*" << input2 << "*" << std::endl;
 
       for(int i=0; i<INSERT_MAX/2; i++) {
         if(i < input2.size() && input2[i] != '\n') {
@@ -179,23 +200,27 @@ int main() {
         }      
       }
 
-      std::cout << "*";
+      std::cout << "Entered \"";
       for(int i=INSERT_MAX/2; i<INSERT_MAX; i++) {
         std::cout << input_chars[i];
       }
-      std::cout << "*";
+      std::cout << "\"";
       std::cout << std::endl;
       
       auto query = TfheString::Encrypt(input_chars, key);
       auto result = TfheArray<int>::Encrypt(raw_result, key);
 
+      std::cout << "\n-- INSERT operation, query ciphertext, and result ciphertext, all sent to server" << std::endl;
+
       for(int i=0; i<db.size();i++) {
         XLS_CHECK_OK(insert(*db[i], query, result, key.cloud()));
       }
 
+      std::cout << "-- Server returned altered result ciphertext\n" << std::endl;
+
       auto output = result.Decrypt(key);
-      std::cout << output[0];
-      std::cout << output[1];
+      std::cout << "Response: ";
+      std::cout << (output[0] ? "Success" : "Failure");
       std::cout << "\n" << std::endl;
     } else if(op_type == 4) {
       int raw_result[1+1] = {0, 0};
@@ -208,7 +233,7 @@ int main() {
       std::cin >> input;
       std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-      std::cout << "*" << input << "*" << std::endl;
+      std::cout << "Entered\"" << input << "\"" << std::endl;
 
       for(int i=0; i<COUNT_MAX; i++) {
         if(i < input.size() && input[i] != '\n') {
@@ -221,12 +246,17 @@ int main() {
       auto query = TfheString::Encrypt(input_chars, key);
       auto result = TfheArray<int>::Encrypt(raw_result, key);
 
+      std::cout << "\n-- COUNT operation, query ciphertext, and result ciphertext, all sent to server" << std::endl;
+
       for(int i=0; i<db.size();i++) {
         XLS_CHECK_OK(count(*db[i], query, result, key.cloud()));
       }
 
+      std::cout << "-- Server returned altered result ciphertext\n" << std::endl;
+
       auto output = result.Decrypt(key);
 
+      std::cout << "Response: ";
       std::cout << output[0];
       std::cout << "\n" << std::endl;
     }
