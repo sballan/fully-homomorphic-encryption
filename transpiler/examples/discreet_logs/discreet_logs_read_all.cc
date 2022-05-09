635f188d17ac80f30f0a60b08c8736a8106f5299
Samuel Ballan <sgb4622@gmail.com>
2022-05-09 02:16:01 +0000

    major bug fix

--- transpiler/examples/discreet_logs/discreet_logs_read_all.cc
+++ transpiler/examples/discreet_logs/discreet_logs_read_all.cc
@@ -4,13 +4,13 @@
 void readAll(
   char record[READ_ALL_MAX+1], 
   int counter[1+1],
-  char result[READ_ALL_COUNT*READ_ALL_COUNT+1]
+  char result[READ_ALL_MAX*READ_ALL_COUNT+1]
 ) {
   // Return if record is blank
   if(record[0] == '\0') {
-    record[READ_ALL_COUNT+1]++;
+    record[READ_ALL_MAX+1]++;
     counter[1]++;
-    result[READ_ALL_COUNT*READ_ALL_COUNT+1]++;
+    result[READ_ALL_MAX*READ_ALL_COUNT+1]++;
     return;
   }
 
